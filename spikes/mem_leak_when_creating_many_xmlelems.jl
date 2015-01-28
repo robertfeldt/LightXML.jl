68b4afc8812b3d8b4f34e33c02ce935d89e7150e
Robert Feldt <robert.feldt@gmail.com>
2015-01-28 08:33:33 +0100

    generalized so we can control finalizer via command line args

--- spikes/mem_leak_when_creating_many_xmlelems.jl
+++ spikes/mem_leak_when_creating_many_xmlelems.jl
@@ -7,7 +7,7 @@ using LightXML
 deltachars = int('z') - int('a')
 randstr(maxlen) = join(map(i->char(int('a') + rand(0:deltachars)), 1:rand(1:maxlen)))
 
-function random_xmlelem_tree(maxdepth = 5)
+function random_xmlelem_tree(maxdepth = 10)
   xmlelement = new_element(randstr(8))
   for r in 1:rand(1:5)
     p = rand()
@@ -22,17 +22,26 @@ function random_xmlelem_tree(maxdepth = 5)
   return xmlelement
 end
 
-size = 0
+function random_xml_doc(maxdepth = 10)
+  xdoc = XMLDocument()
+  set_root(xdoc, random_xmlelem_tree(maxdepth))
+  (length(ARGS) >= 2 && ARGS[2] == "free") ? finalizer(xdoc, free) : nothing
+  xdoc
+end
 
-for i in 1:int(2000)
+sumsizes = 0
+
+for i in 1:2000
   xmls = Any[]
   for j in 1:200
-    push!(xmls, random_xmlelem_tree())
+    x = (length(ARGS) >= 1 && ARGS[1] == "xdoc") ? random_xml_doc() : random_xmlelem_tree()
+    push!(xmls, x)
   end
   # We don't really use the xmls for anything, just check their size and then let go
-  size += sum(map(xe -> length(string(xe)), xmls))
+  sumsizes += sum(map(xe -> length(string(xe)), xmls))
+  print("."); flush(STDOUT);
+  gc() # Should be able to get back all of the xml-related mem here...
 end
-gc()
 
 # When we get here we should only need to have a ref to size left, not to any of the
 # XMLElements. Notify user and sleep so that they can check mem use.
